6e0b674188fd7009843bfee9bc8dfeb8c2ef349f
luboslavistenik <luboslav.is>
2019-11-04 09:04:56 +0100

    Add project files.

---
+++ Open-Lab-2.7/Christmas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_2._7
+{
+    class Christmas
+    {
+        public bool IsTimeForMilkAndCookies(int month, int day)
+        {
+            if (month == 12 && day == 24)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Open-Lab-2.7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Christmas christmas = new Christmas();
+            Console.WriteLine(christmas.IsTimeForMilkAndCookies(12, 24));
+        }
+    }
+}
